292d06073c38407724df0848bc5bb09bf69bbb21
Roja1303 <sroja1559@gmail.com>
2024-10-17 14:26:27 +0530

    problem Solving

--- problem_solving/pascalsTriangle.py
+++ problem_solving/pascalsTriangle.py
@@ -2,10 +2,10 @@ from math import factorial
 
 if __name__ == '__main__':
 
-    num = int(input("Enter the number: "))
+    nums = int(input("Enter the number: "))
 
-    for n in range(num):
-        for space in range(1,num-n):
+    for n in range(nums):
+        for space in range(1,nums-n):
             print(end = " ")
         for r in range(n + 1):
             ncr = factorial(n) // (factorial(r) * factorial(n - r))
